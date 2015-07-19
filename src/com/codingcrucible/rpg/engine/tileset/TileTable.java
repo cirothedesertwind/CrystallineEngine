c76548fa7646f018db08120aedff7bc77c3e7d48
Luke Petrolekas <luke.petrolekas@gmail.com>
2015-07-19 12:06:08 -0400

    Move tileset folder into engine.tileset

--- src/com/codingcrucible/rpg/engine/tileset/TileTable.java
+++ src/com/codingcrucible/rpg/engine/tileset/TileTable.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.codingcrucible.rpg.tileset;
+package com.codingcrucible.rpg.engine.tileset;
 
 import java.awt.image.BufferedImage;
 
